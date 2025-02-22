1d6d1e50b876ab342d7d8a1683fa1f496f1b9374
ozz <oz@gleb.pw>
2025-02-22 18:41:40 +0300

    feat: добавлены плагины для иконок и Vue DevTools
    - Установлен плагин unplugin-icons для автоматической загрузки иконок
    - Добавлен Vue DevTools для улучшения процесса разработки
    - Настроены резолверы для автоматического импорта компонентов

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import IconsResolver from 'unplugin-icons/resolver'
+import {VueUseComponentsResolver} from 'unplugin-vue-components/resolvers'
 import Components from "unplugin-vue-components/vite"
 
 export default defineConfig({
-    plugins: [vue(), Components()],
+    plugins: [
+        vue(),
+        VueDevTools(),
+        Icons({
+            compiler: 'vue3',
+            autoInstall: true,
+            scale: 1.5
+        }),
+        Components({
+            dirs: ['src/*'],
+            extensions: ['vue'],
+            resolvers: [
+                IconsResolver(),
+                VueUseComponentsResolver()
+            ],
+        })
+    ],
 })
