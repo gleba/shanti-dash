5ecbcc499ae240e9052978c23f2a0a8159ae05c9
ozz <oz@gleb.pw>
2025-02-22 15:36:34 +0300

    feat: добавить базовый WebSocket сервер и ослабить проверки TypeScript
    Подробное описание изменений:
    • Создан новый файл server.ts с базовой реализацией WebSocket сервера
    • Отключены строгие проверки TypeScript (noUnusedLocals, noUnusedParameters)
    • Добавлено отображение точки для открытых элементов в ScheduleRow
    • Добавлены стили для временных меток в расписании
    
    Технические детали:
    • Сервер настроен на порт 3000
    • Добавлена поддержка SPA через раздачу index.html
    • Реализована базовая WebSocket коммуникация

--- src/components/ScheduleRow.vue
+++ src/components/ScheduleRow.vue
@@ -15,7 +15,8 @@ function select() {
       .time {{ i.time }}
       .name {{ i.title }}
       .count {{ i.participantsCount }}
-      .button ^
+      .button(v-if="i.isOpen") .
+      .button(v-else) ^
     .heh
     .detail(v-if="i.isOpen")
       .description(v-html="i.description")
@@ -27,5 +28,12 @@ function select() {
 .debug {
   font-family: monospace;
   opacity: .2;
+}
+.time {
+  //text-align: center;
+}
+.end {
+  opacity: .2;
+  font-size: 13px;
 }
 </style>
