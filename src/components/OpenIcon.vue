a0a5676f6b1c33e11a2a4dd7258405b2fbdf426d
mercurian-kitty <mercurianvalerie@yandex.ru>
2025-02-22 21:04:22 +0300

    +++++

---
+++ src/components/OpenIcon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps(["isOpen"]);
+</script>
+
+<template lang="pug">
+ISolarAltArrowUpLineDuotone(v-if="isOpen")
+ISolarAltArrowDownLineDuotone(v-else)
+</template>
+
+<style scoped>
+
+</style>
